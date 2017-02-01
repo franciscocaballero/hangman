5b7ab05fc1bb4dd1435a226fec6abd026eb1207a
xgironx <xgironx@gmail.com>
2017-02-01 13:44:43 -0500

    commit after demo where we found winner logic error, before starting to fix it

--- hangman.js
+++ hangman.js
@@ -1,103 +1,104 @@
-                          // 201701310918L   EL MARTES   GIRO
-                          //SETFOCUS TO THE FIRST FIELD SO I DONT HAVE TO CLICK IN IT
-                          // $("#playerOnesWord").focus(function(){ $("span").css("display", "inline").fadeOut(2000); });
+    // 201701310918L   EL MARTES   GIRO
+    //SETFOCUS TO THE FIRST FIELD SO I DONT HAVE TO CLICK IN IT
+    // $("#playerOnesWord").focus(function(){ $("span").css("display", "inline").fadeOut(2000); });
 
-                          $(document).ready(function() { //console.log("Hangman Game");
+    $(document).ready(function() { //console.log("Hangman Game");
 
 
-                          var btnWord = $("#btnWord");
-                          var inputWord = $("#inputWord");
-                          var theWord = 'adf';
-                          var arrTheWord;
-                          var btnGuess = $("#btnGuess");
-                          var inputGuess = $("#inputGuess");
-                          var guessRight = 0;
-                          var guessWrong = 0;
+    var btnWord = $("#btnWord");
+    var inputWord = $("#inputWord");
+    var theWord = 'adf';
+    var arrTheWord;
+    var btnGuess = $("#btnGuess");
+    var inputGuess = $("#inputGuess");
+    var guessRight = 0;
+    var guessWrong = 0;
 
-                          btnWord.on('click', function(event) {
-                            event.preventDefault();
-                            //console.log("in click handler btnWord");
-                            //console.log(inputWord.val());
-                            theWord = inputWord.val();
-                            //console.log("heres theWord:" + theWord);
-                            arrTheWord = theWord.split(/(?!$)/);
-                            //this works: //"overpopulation".split(/(?!$)/)
-                            //console.log("heres arrTheWord:" + arrTheWord);
-                            fillVannaWhite();
-                          });
+    btnWord.on('click', function(event) {
+      event.preventDefault();
+      //console.log("in click handler btnWord");
+      //console.log(inputWord.val());
+      theWord = inputWord.val();
+      //console.log("heres theWord:" + theWord);
+      arrTheWord = theWord.split(/(?!$)/);
+      //this works: //"overpopulation".split(/(?!$)/)
+      //console.log("heres arrTheWord:" + arrTheWord);
+      fillVannaWhite();
+    });
 
-                          function fillVannaWhite() {
-                            //console.log('filling in Vanna');
-                            theWordCss = $("#theWord");
-                            for (i = 0; i < arrTheWord.length; i++) {
-                              var thisLetter = arrTheWord[i]; //console.log('thisLetter:' + thisLetter);
 
-                              var div = $('<div id="div' + i + '"  class="thisGuess" > ' + thisLetter + ' </div>');
+    function fillVannaWhite() {
+      //console.log('filling in Vanna');
+      theWordCss = $("#theWord");
+      for (i = 0; i < arrTheWord.length; i++) {
+        var thisLetter = arrTheWord[i]; //console.log('thisLetter:' + thisLetter);
 
-                              theWordCss.append(div);
-                            }
-                          }
+        var div = $('<div id="div' + i + '"  class="thisGuess" > ' + thisLetter + ' </div>');
 
-                          btnGuess.on('click', function(event) {
-                            event.preventDefault();
-                            //console.log(inputGuess.val());
-                            thisGuess = inputGuess.val();
-                            comparo(thisGuess);
-                          });
-                          var arrTheWordSoFar = [];
+        theWordCss.append(div);
+      }
+    }
 
-                          function comparo(thisGuess) {
-                            //THIS IS CASE-SENSITIVE
-                            //console.log('in comparo with the guess: ' + thisGuess);
-                            //console.log('thisWord: ' + theWord);
-                            //console.log('thisGuess:' + thisGuess);
-                            if ($.inArray(thisGuess, arrTheWord) !== -1) {
-                              //console.log('thisGuess:' + thisGuess + ' is in thisWord: ' + theWord);
-                              //loop over array finding all the instances of the current character
-                              //console.log("Length of arrTheWord: " + arrTheWord.length);
-                              for (i = 0; i < arrTheWord.length; i++) {
-                                //console.log("i: " + i);
-                                //console.log("char: " + arrTheWord[i]);
-                                if (arrTheWord[i] === thisGuess) {
-                                  guessRight = guessRight + 1;
-                                  console.log('guessRight: ' + guessRight);
-                                  //console.log("Found " + thisGuess + " at index " + i);
-                                  id = "#div" + i;
-                                  //console.log("div id: " + id);
-                                  matchDiv = $(id);
-                                  // if (matchDiv)   //console.log("we have the matching div");
-                                  matchDiv.css({ 'background-color': '#ff0000', 'color': '#ffffff' });
-                                  // var numbers = new Set([1, 2]); // Set {1, 2, 4}
-                                  //         								  //console.log(numbers);
-                                  // numbers.add(4);           // Set {1, 2, 4}
-                                  //         								  //console.log(numbers);
-                                  // numbers.add(6);           // Set {1, 2, 4, 6}
-                                  //         								  //console.log(numbers);
-                                  arrTheWordSoFar.push(thisGuess);
-                                  //console.log("heres arrTheWordSoFar:" + arrTheWordSoFar);
-                                  //console.log("heres arrTheWordSoFar:" + arrTheWordSoFar.indexOf(i));
-                                  //check if lengths are different
-                                  // if (arrTheWord.length   !== arrTheWordSoFar.length) {
-                                  // if (arrTheWord.toString !== arrTheWordSoFar.toString) {
-                                  // console.log("aint no winner yet");
-                                } else {
-                                  guessWrong = guessWrong + 1;
-                                //  console.log('guessWrong: ' + guessWrong);
-                                }
-                              //  console.log('arrTheWord.length: ' + arrTheWord.length);
-                                if (guessRight == arrTheWord.length) {
-                                  console.log('equal');
-                                  console.log('happy day, mo fo');
-                                  alert("Oh Happy Day!");
-                                }
-                              }
-                            }
-                            else {
-                              alert("Oops try again!");
-                            }
-                          }
-                        }
-                      )
-                          //   }                             //$('#thisGuess').append('+');    //console.log('were on div: ' + i);
-                          // } else {
-                          //               								  //console.log('keep trying suckah');
+    btnGuess.on('click', function(event) {
+      event.preventDefault();
+      //console.log(inputGuess.val());
+      thisGuess = inputGuess.val();
+      comparo(thisGuess);
+    });
+    var arrTheWordSoFar = [];
+
+    function comparo(thisGuess) {
+      //THIS IS CASE-SENSITIVE
+      //console.log('in comparo with the guess: ' + thisGuess);
+      //console.log('thisWord: ' + theWord);
+      //console.log('thisGuess:' + thisGuess);
+      if ($.inArray(thisGuess, arrTheWord) !== -1) {
+        //console.log('thisGuess:' + thisGuess + ' is in thisWord: ' + theWord);
+        //loop over array finding all the instances of the current character
+        //console.log("Length of arrTheWord: " + arrTheWord.length);
+        for (i = 0; i < arrTheWord.length; i++) {
+          //console.log("i: " + i);
+          //console.log("char: " + arrTheWord[i]);
+          if (arrTheWord[i] === thisGuess) {
+            guessRight = guessRight + 1;
+            console.log('guessRight: ' + guessRight);
+            //console.log("Found " + thisGuess + " at index " + i);
+            id = "#div" + i;
+            //console.log("div id: " + id);
+            matchDiv = $(id);
+            // if (matchDiv)   //console.log("we have the matching div");
+            matchDiv.css({ 'background-color': '#ff0000', 'color': '#ffffff' });
+            // var numbers = new Set([1, 2]); // Set {1, 2, 4}
+            //         								  //console.log(numbers);
+            // numbers.add(4);           // Set {1, 2, 4}
+            //         								  //console.log(numbers);
+            // numbers.add(6);           // Set {1, 2, 4, 6}
+            //         								  //console.log(numbers);
+            arrTheWordSoFar.push(thisGuess);
+            //console.log("heres arrTheWordSoFar:" + arrTheWordSoFar);
+            //console.log("heres arrTheWordSoFar:" + arrTheWordSoFar.indexOf(i));
+            //check if lengths are different
+            // if (arrTheWord.length   !== arrTheWordSoFar.length) {
+            // if (arrTheWord.toString !== arrTheWordSoFar.toString) {
+            // console.log("aint no winner yet");
+          } else {
+            guessWrong = guessWrong + 1;
+          //  console.log('guessWrong: ' + guessWrong);
+          }
+        //  console.log('arrTheWord.length: ' + arrTheWord.length);
+          if (guessRight == arrTheWord.length) {
+            console.log('equal');
+            console.log('happy day, mo fo');
+            alert("Oh Happy Day!");
+          }
+        }
+      }
+      else {
+        alert("Oops try again!");
+      }
+    }
+  }
+)
+    //   }                             //$('#thisGuess').append('+');    //console.log('were on div: ' + i);
+    // } else {
+    //               								  //console.log('keep trying suckah');
